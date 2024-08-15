6530181d205773c87f0b62172505e729b1a20b4e
roshidoniy <samadbek.mahmudjonov@gmail.com>
2024-08-15 18:03:40 +0500

    Showcase & fixed little bugs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import DropFileInput from "./components/DropFileInput";
+const App2 = () => {
+  return (
+    <div className="box">
+      <h1 className="header">Advancedt
+         Drag & Drop</h1>
+      <DropFileInput />
+    </div>
+  );
+};
+
+export default App2;
